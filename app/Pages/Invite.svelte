0c8aa1eac6f33b37a3d6c312ab76223a3a3d9704
Harry <harryyork97@gmail.com>
2021-07-27 16:52:26 +0100

    add in Invite page

---
+++ app/Pages/Invite.svelte
@@ -0,0 +1,28 @@
+<script lang="typescript">
+  // import Goalpage from "./Goalpage.svelte";
+  // import {navigate} from "svelte-native";
+</script>
+
+<page>
+  <stackLayout>
+    <label
+      fontSize="35"
+      color="white"
+      text="Invite your friends!"
+      textWrap="true"
+    />
+    <label
+      fontSize="35"
+      color="white"
+      text="The more friends you invite to your goal, the more likely you are to achieve."
+      textWrap="true"
+    />
+
+    <button text="Invite your friends" />
+
+    <button text="Go to Homepage" />
+  </stackLayout>
+</page>
+
+<style>
+</style>
