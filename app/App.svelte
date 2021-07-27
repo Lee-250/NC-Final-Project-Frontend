311af05ba31745d4b9547869aa1d4a3e6032eb91
Harry <harryyork97@gmail.com>
2021-07-27 16:40:51 +0100

    add in SetupComplete

--- app/App.svelte
+++ app/App.svelte
@@ -1,23 +1,23 @@
 <script lang="typescript">
-        
-    import Start from "./Pages/Start.svelte";
-    
-    import {navigate} from "svelte-native";
-    
-    let message: string = "Blank Svelte Native App";
+  import SetupComplete from "./Pages/SetupComplete.svelte";
 
+  import { navigate } from "svelte-native";
+
+  let message: string = "Blank Svelte Native App";
 </script>
-   
-
-<style>
-    
-</style>
-
 
 <page>
-<stackLayout paddingBottom ="80">
-   <label text="Achieve" fontSize="70" horizontalAlignment="center" color ="white" />
-   <image src="~/Images/trophy.png" />
-   <button on:tap="{() =>navigate({page: Start})}" text="Start" ></button>
-</stackLayout>
+  <stackLayout paddingBottom="80">
+    <label
+      text="Achieve"
+      fontSize="70"
+      horizontalAlignment="center"
+      color="white"
+    />
+    <image src="~/Images/trophy.png" />
+    <button on:tap={() => navigate({ page: SetupComplete })} text="Start" />
+  </stackLayout>
 </page>
+
+<style>
+</style>
