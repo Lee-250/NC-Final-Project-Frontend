f7f6bc15997f308d0a30f94ed9670d89dabbfc31
JohnA-NC <80391624+JohnA-NC@users.noreply.github.com>
2021-07-27 16:14:09 +0100

    added an info page

--- app/Pages/Start.svelte
+++ app/Pages/Start.svelte
@@ -1,6 +1,6 @@
 <script lang="typescript">
-//import  from "./Pages/Start.svelte";
-//import {navigate} from "svelte-native";
+import Info from "./Info.svelte";
+import {navigate} from "svelte-native";
 </script>
 
 <style>
@@ -12,11 +12,11 @@
     <actionBar text="intro"></actionBar>
     <stackLayout>
             <label text="First time here?" fontSize="50"></label>
-            <button text="Lets start"></button>
+            <button on:tap="{() =>navigate({page: Info})}" text="Lets start"></button>
             <label text="Returning user" fontSize="50"></label>
             <textField class="input" hint="email"></textField>
             <textField class="input" hint="username"></textField>
             <textField class="input" hint="password"></textField>
-            <!-- <button on:tap="{() =>navigate({page: Info})}" text="Start" ></button> -->
+            <button text="Start" ></button>
     </stackLayout>
 </page>
