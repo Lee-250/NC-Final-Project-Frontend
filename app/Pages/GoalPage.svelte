cb47d3d9d18d32c80e2f42baad35adc10644b28c
Lee Broadhurst <lee_b250@hotmail.co.uk>
2021-07-28 15:23:52 +0100

    complete main structure of goalpage

--- app/Pages/GoalPage.svelte
+++ app/Pages/GoalPage.svelte
@@ -1,12 +1,15 @@
 <script lang="typescript"> 
     import {Template} from 'svelte-native/components'
- let updates: { user: string, post: string }[] = [{user: 'Chris', post: 'Quick 5 miles'},{user: 'John', post: 'Big ride planned this weekend!!'}]
+ let updates: { user: string, post: string }[] = [{user: 'Lee', post: 'Just got back from a 450 mile ride, too easy'}, {user: 'Harry', post: 'I love bikes me'}, {user: 'Chris', post: 'Quick 5 miles'},{user: 'John', post: 'Big ride planned this weekend!!'}]
 
- let newUpdate: { user: string, post: string } = {user: 'Lee', post: 'Lets go!'};
- let post: string = "";
+//  let newUpdate: { user: string, post: string } = {user: 'Lee', post: ''};
+ let newPost: { user: string, post: string } = {user: 'Lee', post: ''};
+ 
 
- const addUpdate = () => {
-    updates = [...updates, newUpdate]
+ const addUpdate = ():void => {
+    updates = [{user: newPost.user, post: newPost.post}, ...updates];
+    newPost.post = '';
+    
  }
 
 
@@ -34,10 +37,11 @@
     <gridLayout columns="3*, *" class="progressbar">
         <stackLayout col="0" class="progressbar-value"></stackLayout>
       </gridLayout>
-      <textField bind:text="{newUpdate.post}" hint="post an update"></textField>
-      <!-- test -->
+      <!-- Need to make hint faded -->
+      <textField bind:text="{newPost.post}" hint="post an update" ></textField>
       <button on:tap="{addUpdate}"text="Post"></button>
-    <listView height="200" items="{updates}">
+      <!-- need to make height full screen -->
+    <listView height="600" items="{updates}"> 
     <Template let:item>
         <label text="{item.user}" />
         <label text="{item.post}" />
