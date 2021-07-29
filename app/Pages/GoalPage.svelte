e3323ec9a190b1eee7f8cbfae7cb3b7603f6e17d
JohnA-NC <80391624+JohnA-NC@users.noreply.github.com>
2021-07-29 16:25:06 +0100

    fetched from API

--- app/Pages/GoalPage.svelte
+++ app/Pages/GoalPage.svelte
@@ -1,10 +1,19 @@
 <script lang="typescript"> 
-    import {Template} from 'svelte-native/components'
+import { BottomNavigation, StackLayout, TabStripItem } from '@nativescript/core';
+import axios from 'axios/dist/axios';
+import {Template} from 'svelte-native/components';
+
  let updates: { user: string, post: string }[] = [{user: 'Lee', post: 'Just got back from a 450 mile ride, too easy'}, {user: 'Harry', post: 'I love bikes me'}, {user: 'Chris', post: 'Quick 5 miles'},{user: 'John', post: 'Big ride planned this weekend!!'}]
 
-//  let newUpdate: { user: string, post: string } = {user: 'Lee', post: ''};
  let newPost: { user: string, post: string } = {user: 'Lee', post: ''};
  
+ const testApi = axios.create({baseURL: "https://mygames-api.herokuapp.com/api"});
+ const getReviews = async () => {
+     const {data}  = await testApi.get('/reviews')
+     return data.reviews;
+};
+
+getReviews();
 
  const addUpdate = ():void => {
     updates = [{user: newPost.user, post: newPost.post}, ...updates];
@@ -32,24 +41,39 @@
 </style>
 
 <page>
-<stackLayout>
-    <label text="GoalPage" fontSize="50"/>
-    <gridLayout columns="3*, *" class="progressbar">
-        <stackLayout col="0" class="progressbar-value"></stackLayout>
-      </gridLayout>
-      <!-- Need to make hint faded -->
-      <textField bind:text="{newPost.post}" hint="post an update" ></textField>
-      <button on:tap="{addUpdate}"text="Post"></button>
-      <!-- need to make height full screen -->
-    <listView height="600" items="{updates}"> 
-    <Template let:item>
-        <label text="{item.user}" />
-        <label text="{item.post}" />
-    </Template>
-    </listView>
-
-
-</stackLayout>
-
+    <stackLayout>
+        <bottomNavigation>
+            <tabStrip>
+                <tabStripItem>
+                    <label text="Home" />
+                </tabStripItem>
+                <tabStripItem>
+                    <label text="Progress" />
+                </tabStripItem>
+            </tabStrip>
+            <tabContentItem backgroundColor="#92cd92">
+                <stackLayout class="m-x-auto" backgroundColor="#E9FDE3">
+                    <label class="text-center" text="GoalPage" fontSize="50"/>
+                    <gridLayout columns="3*, *" class="progressbar">
+                        <stackLayout col="0" class="progressbar-value"></stackLayout>
+                      </gridLayout>
+                      <!-- Need to make hint faded -->
+                      <textField bind:text="{newPost.post}" hint="post an update" ></textField>
+                      <button on:tap="{addUpdate}"text="Post"></button>
+                      <button on:tap="{getReviews}"text="Reviews"></button>
+                      <!-- need to make height full screen -->
+                    <listView height="600" items="{updates}"> 
+                    <Template let:item>
+                        <label text="{item.user}" />
+                        <lavel text="{item.post}" />
+                    </Template>
+                    </listView>
+                </stackLayout>
+            </tabContentItem>
+            <tabContentItem backgroundColor="#92cd92">
+                <label text="progress" />
+            </tabContentItem>
+        </bottomNavigation>
+        </stackLayout>
 
 </page>
