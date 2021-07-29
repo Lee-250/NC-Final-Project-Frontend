364c0b591b5ff875dba72665d118e87fffcffe04
Lee Broadhurst <lee_b250@hotmail.co.uk>
2021-07-29 17:05:16 +0100

    add get request to dev api

--- app/Pages/GoalPage.svelte
+++ app/Pages/GoalPage.svelte
@@ -1,19 +1,30 @@
 <script lang="typescript"> 
 import { BottomNavigation, StackLayout, TabStripItem } from '@nativescript/core';
+import { dateProperty } from '@nativescript/core/ui/date-picker';
 import axios from 'axios/dist/axios';
+import { onMount } from 'svelte';
 import {Template} from 'svelte-native/components';
 
  let updates: { user: string, post: string }[] = [{user: 'Lee', post: 'Just got back from a 450 mile ride, too easy'}, {user: 'Harry', post: 'I love bikes me'}, {user: 'Chris', post: 'Quick 5 miles'},{user: 'John', post: 'Big ride planned this weekend!!'}]
 
  let newPost: { user: string, post: string } = {user: 'Lee', post: ''};
  
- const testApi = axios.create({baseURL: "https://mygames-api.herokuapp.com/api"});
- const getReviews = async () => {
-     const {data}  = await testApi.get('/reviews')
-     return data.reviews;
-};
+//  const testApi = axios.create({baseURL: "https://mygames-api.herokuapp.com/api"});
 
-getReviews();
+ const devApi = axios.create({baseURL: "https://us-central1-final-project-backend-16738.cloudfunctions.net/app"})
+
+let feed = [];
+
+ onMount(async () => {
+     
+         const {data}  = await devApi.get('/feed')
+         feed = data;
+    
+})
+
+
+
+
 
  const addUpdate = ():void => {
     updates = [{user: newPost.user, post: newPost.post}, ...updates];
@@ -60,12 +71,12 @@ getReviews();
                       <!-- Need to make hint faded -->
                       <textField bind:text="{newPost.post}" hint="post an update" ></textField>
                       <button on:tap="{addUpdate}"text="Post"></button>
-                      <button on:tap="{getReviews}"text="Reviews"></button>
+                      
                       <!-- need to make height full screen -->
-                    <listView height="600" items="{updates}"> 
+                    <listView height="600" items="{feed}"> 
                     <Template let:item>
-                        <label text="{item.user}" />
-                        <lavel text="{item.post}" />
+                        <label text="{item.messageBody}" />
+                        
                     </Template>
                     </listView>
                 </stackLayout>
